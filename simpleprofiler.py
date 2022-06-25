1f7c0cd8d4d05129307ef2b26659e31de13e9c27
Hans Teh <73771314+HansT01@users.noreply.github.com>
2022-06-25 12:50:00 +1000

    Added profile plotting prototype

--- simpleprofiler.py
+++ simpleprofiler.py
@@ -2,7 +2,9 @@ from functools import wraps
 import inspect
 import sys
 from time import perf_counter
-from typing import Dict, Any, Callable
+from typing import Dict, Any, Callable, List, Tuple
+from matplotlib import pyplot as plt
+import numpy as np
 from tabulate import tabulate
 from operator import itemgetter
 
@@ -10,7 +12,7 @@ from operator import itemgetter
 class TimeProfiler:
     """TimeProfiler is a class for quickly storing the time taken for each method to complete, and displaying it as an easy-to-read table."""
 
-    profiles = {}
+    profiles: Dict[Callable, List[Tuple[float, float]]] = {}
 
     ORDER_BY_NAME = 0
     ORDER_BY_CALLS = 1
@@ -61,9 +63,9 @@ class TimeProfiler:
         Args:
             order_by (int, optional): Optional ordering using provided ORDER_BY_ fields. Defaults to ORDER_BY_NAME.
         """
-        times: Dict[Callable, Any] = TimeProfiler.profiles
+        profiles = TimeProfiler.profiles
         table = []
-        for key in times:
+        for key in profiles:
             row = []
 
             longest = 0
@@ -72,7 +74,7 @@ class TimeProfiler:
             earliest = sys.maxsize
             latest = 0
 
-            for start, end in times[key]:
+            for start, end in profiles[key]:
                 elapsed = end - start
                 sum += elapsed
                 if elapsed > longest:
@@ -82,7 +84,7 @@ class TimeProfiler:
                 if end > latest:
                     latest = end
 
-            calls = len(times[key])
+            calls = len(profiles[key])
             avg = sum / calls
             total = latest - earliest
 
@@ -113,6 +115,69 @@ class TimeProfiler:
             )
         )
 
+    @staticmethod
+    def plot_profiles():
+        earliest, latest = TimeProfiler.__get_time_range()
+        new_profiles = TimeProfiler.__squash_profiles(earliest, latest)
+        TimeProfiler.__plot_data(new_profiles, 0, latest - earliest)
+
+    @staticmethod
+    def __get_time_range() -> Tuple[float, float]:
+        profiles = TimeProfiler.profiles
+
+        # Get earliest and latest times
+        earliest = sys.maxsize
+        latest = 0
+        for key in profiles:
+            for start, end in profiles[key]:
+                if start < earliest:
+                    earliest = start
+                if end > latest:
+                    latest = end
+
+        return earliest, latest
+
+    @staticmethod
+    def __squash_profiles(
+        earliest: float,
+        latest: float,
+    ) -> Dict[str, List[Tuple[float, float]]]:
+
+        profiles = TimeProfiler.profiles
+        new_profiles: Dict[str, List[Tuple[float, float]]] = {}
+        time_frame = latest - earliest
+
+        # Fill in new_profiles with normalized times
+        for key in profiles:
+            new_key = key.__name__
+            if new_key not in new_profiles:
+                new_profiles[new_key] = []
+
+            for start, end in profiles[key]:
+                new_start = (start - earliest) / time_frame
+                new_end = (end - earliest) / time_frame
+                new_profiles[new_key] += [(new_start, new_end)]
+
+        return new_profiles
+
+    @staticmethod
+    def __plot_data(
+        data: Dict[Callable, List[Tuple[float, float]]], ymin: float, ymax: float
+    ):
+        fig, ax = plt.subplots()
+        width = 1
+
+        ax.set_ylim(ymin, ymax)
+
+        for i, pair in enumerate(data.items()):
+            for value in pair[1]:
+                y0, y1 = value
+                ax.axvspan(xmin=i - width / 2, xmax=i + width / 2, ymin=y0, ymax=y1)
+
+        ax.set_xticks(np.arange(0, len(data)))
+        ax.set_xticklabels(data.keys())
+        plt.show()
+
 
 if __name__ == "__main__":
 
@@ -121,22 +186,27 @@ if __name__ == "__main__":
 
     @TimeProfiler.profile_class_methods
     class ExampleClass:
+        def method_b(self):
+            sleep(randint(0, 1000) / 10000)
+
         @staticmethod
         def method_a():
             sleep(randint(0, 1000) / 10000)
 
-        def method_b(self):
-            sleep(randint(0, 1000) / 10000)
-
         def method_c(self):
-            sleep(randint(0, 1000) / 10000)
+            sleep(randint(0, 2000) / 10000)
 
     calc = ExampleClass()
+
     for _ in range(0, 5):
         ExampleClass.method_a()
+        calc.method_b()
+
+    for _ in range(0, 5):
         calc.method_b()
 
     for _ in range(0, 5):
         calc.method_c()
 
     TimeProfiler.display_profiles(TimeProfiler.ORDER_BY_NAME)
+    TimeProfiler.plot_profiles()
